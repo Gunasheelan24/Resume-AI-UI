71d196130aba5e39f0370ed9ba686bc68ac8f253
Gunasheelan Arullchelvan <140253087+Gunasheelan24@users.noreply.github.com>
2026-08-05 21:42:22 +0530

    Update button.tsx

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -1,7 +1,6 @@
 import * as React from "react"
 import { cva, type VariantProps } from "class-variance-authority"
 import { Slot } from "radix-ui"
-
 import { cn } from "@/lib/utils"
 
 const buttonVariants = cva(
@@ -40,6 +39,8 @@ const buttonVariants = cva(
     },
   }
 )
+
+
 
 function Button({
   className,
